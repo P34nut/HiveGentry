b1500cb0bdbc5f372222473125f37212c0fec06e
Linus Ehmann <linus.ehmann@gmx.de>
2025-11-07 23:52:51 +0100

    ADDED clamp and evenly distribution of energy

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
@@ -30,12 +31,21 @@ public class GameManager : MonoBehaviour
     
     public void TransferEnergy (Room currentRoom)
     {
+        if (currentRoom.Energy >= 100)
+            return;
+        
         currentRoom.Energy += EnergyTransferRate * Time.deltaTime;
         currentRoom.Refresh();
+
+        var activeRoomCounter = Rooms.Count(r => r != currentRoom && r.Energy > 0);
+        
         foreach (Room room in Rooms)
         {
             if (room == currentRoom) continue;
-            room.Energy -= EnergyTransferRate * Time.deltaTime / (Rooms.Count - 1);
+            if (room.Energy <= 0) continue;
+            
+            room.Energy -= EnergyTransferRate * Time.deltaTime / activeRoomCounter;
+            room.Energy = Mathf.Clamp(room.Energy, 0, 100);
             room.Refresh();
         }
     }
