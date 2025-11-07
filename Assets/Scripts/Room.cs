7e299ddb272cb47137752f04f03ba69097a6e1f1
SlaughteredUnicorn <SlaughteredUnicorn>
2025-11-07 23:09:26 +0100

    new not working buttons

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
 using System;
+using TMPro;
 
 public class Room : MonoBehaviour
 {
     public string Name;
     public float Energy;
+    public TMP_Text Label;
 
+    public void Refresh ()
+    {
+        Label.text = Name + ": " + Energy + "%";
+    }
+
+    public void TransferEnergy()
+    {
+        GameManager.Instance.TransferEnergy(this);
+    }
 }
