095d2b47fdc44625531474323edfdb9ea5b091c7
SlaughteredUnicorn <SlaughteredUnicorn>
2025-11-09 16:22:01 +0100

    add intro and tutorial

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,9 +1,15 @@
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject Overlay;
+    public TMP_Text text1;
+    public TMP_Text text2;
+
     public void OnQuitClicked()
     {
 #if UNITY_EDITOR
@@ -12,9 +18,40 @@ public class MainMenu : MonoBehaviour
         Application.Quit ();
 #endif
     }
-    
+
     public void OnStartGame()
     {
+        StartCoroutine(StartGameEnumerator());
+    }
+    
+    private IEnumerator StartGameEnumerator ()
+    {
+        Overlay.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        text1.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        text2.gameObject.SetActive(true);
+
+        text2.text = "Hive Gentry is connected...";
+yield return new WaitForSeconds(1f);
+        text2.text = "Hive entryG is connected...";
+yield return new WaitForSeconds(0.3f);
+        text2.text = "ive entHryG is connected...";
+yield return new WaitForSeconds(0.3f);
+        text2.text = "evi entHryG is connected...";
+yield return new WaitForSeconds(0.3f);
+        text2.text = "evei ntHryG is connected...";
+yield return new WaitForSeconds(0.3f);
+        text2.text = "everi ntHyG is connected...";
+yield return new WaitForSeconds(0.3f);
+        text2.text = "everyi ntHG is connected...";
+yield return new WaitForSeconds(0.3f);
+        text2.text = "everytHi nG is connected...";
+yield return new WaitForSeconds(0.3f);
+        text2.text = "everytHinG is connected...";
+yield return new WaitForSeconds(0.3f);
+        text2.text = "everytHinG is connected.";
+        yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene(sceneBuildIndex: 1);
     }
 }
